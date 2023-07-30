b0b72f8ff698eaf310698f6e3097b036a7f5a868
yashkorde13 <yash.korde13@gmail.com>
2023-07-30 14:45:47 +0530

    Courses component added

---
+++ src/component/courses/Courses.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+const Home: React.FC = () =>{
+    return(
+        <>
+            <Navbar/>
+            <h2 className={"sample-text text-center p-4 mt-lg-4 "}>In cricket, the term wicket has several meanings:
+
+                It is one of the two sets of three stumps and two bails at either end of the pitch.[1] The fielding team's players can hit the wicket with the ball in a number of ways to get a batter out.
+                The wicket is guarded by a batter who, with their bat (and sometimes with their pads, but see the laws on LBW, leg before wicket), attempts to prevent the ball from hitting the wicket (if it does, he is bowled out) and to score runs where possible.
+                Through metonymic usage, the dismissal of a batter is known as the taking of a wicket,[2]
+                The cricket pitch itself is sometimes referred to as the wicket.[3][4]
+                In croquet, wooden or plastic balls are hit with a mallet through hoops embedded in a grass playing court. A hoop is commonly referred to as a wicket in North American English.
+                The wicket can be thought of as the target of the fielding team, as the bowler and fielders alike can dismiss the batter by hitting the wicket with the ball, and in particular, can prevent run-scoring (off a ball that has not reached the boundary) by managing or threatening to run out batters.
+
+                For a batter to be dismissed by being bowled, run out, stumped or hit wicket, their wicket needs to be put down, potentially when neither batter is in the ground of the wicket. This generally occurs when a fielder throws the ball at the wicket, or hits it with ball in hand. What this means is defined by Law 29. A wicket is put down if:
+
+                A bail is completely removed from the top of the stumps
+                A stump is struck out of the grounds by the ball, the striker's bat, the striker's person (or by any part of his clothing or equipment becoming detached from his person), a fielder (with his hand or arm, and provided that the ball is held in the hand or hands so used, or in the hand of the arm so used).
+                A 2010 amendment to the Laws clarified the rare circumstance where a bat breaks during the course of a shot and the detached debris breaks the wicket; the wicket has been put down in this circumstance.[7]
+                The wicket is also put down if a fielder pulls a stump out of the ground in the same manner.
+                The wicket can be thought of as the target of the fielding team, as the bowler and fielders alike can dismiss the batter by hitting the wicket with the ball, and in particular, can prevent run-scoring (off a ball that has not reached the boundary) by managing or threatening to run out batters.
+
+                For a batter to be dismissed by being bowled, run out, stumped or hit wicket, their wicket needs to be put down, potentially when neither batter is in the ground of the wicket. This generally occurs when a fielder throws the ball at the wicket, or hits it with ball in hand. What this means is defined by Law 29. A wicket is put down if:
+
+                A bail is completely removed from the top of the stumps
+                A stump is struck out of the grounds by the ball, the striker's bat, the striker's person (or by any part of his clothing or equipment becoming detached from his person), a fielder (with his hand or arm, and provided that the ball is held in the hand or hands so used, or in the hand of the arm so used).
+                A 2010 amendment to the Laws clarified the rare circumstance where a bat breaks during the course of a shot and the detached debris breaks the wicket; the wicket has been put down in this circumstance.[7]
+                The wicket is also put down if a fielder pulls a stump out of the ground in the same manner.
+                The wicket can be thought of as the target of the fielding team, as the bowler and fielders alike can dismiss the batter by hitting the wicket with the ball, and in particular, can prevent run-scoring (off a ball that has not reached the boundary) by managing or threatening to run out batters.
+
+                For a batter to be dismissed by being bowled, run out, stumped or hit wicket, their wicket needs to be put down, potentially when neither batter is in the ground of the wicket. This generally occurs when a fielder throws the ball at the wicket, or hits it with ball in hand. What this means is defined by Law 29. A wicket is put down if:
+
+                A bail is completely removed from the top of the stumps
+                A stump is struck out of the grounds by the ball, the striker's bat, the striker's person (or by any part of his clothing or equipment becoming detached from his person), a fielder (with his hand or arm, and provided that the ball is held in the hand or hands so used, or in the hand of the arm so used).
+                A 2010 amendment to the Laws clarified the rare circumstance where a bat breaks during the course of a shot and the detached debris breaks the wicket; the wicket has been put down in this circumstance.[7]
+                The wicket is also put down if a fielder pulls a stump out of the ground in the same manner.
+                The wicket can be thought of as the target of the fielding team, as the bowler and fielders alike can dismiss the batter by hitting the wicket with the ball, and in particular, can prevent run-scoring (off a ball that has not reached the boundary) by managing or threatening to run out batters.
+
+                For a batter to be dismissed by being bowled, run out, stumped or hit wicket, their wicket needs to be put down, potentially when neither batter is in the ground of the wicket. This generally occurs when a fielder throws the ball at the wicket, or hits it with ball in hand. What this means is defined by Law 29. A wicket is put down if:
+
+                A bail is completely removed from the top of the stumps
+                A stump is struck out of the grounds by the ball, the striker's bat, the striker's person (or by any part of his clothing or equipment becoming detached from his person), a fielder (with his hand or arm, and provided that the ball is held in the hand or hands so used, or in the hand of the arm so used).
+                A 2010 amendment to the Laws clarified the rare circumstance where a bat breaks during the course of a shot and the detached debris breaks the wicket; the wicket has been put down in this circumstance.[7]
+                The wicket is also put down if a fielder pulls a stump out of the ground in the same manner.
+            </h2>
+            <Footer/>
+        </>
+    )
+}
+export default Home;
